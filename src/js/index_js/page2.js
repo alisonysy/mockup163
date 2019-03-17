40f64e75b80505ef91c0b0e15302867251f7881a
alisonys <qwen735@aucklanduni.ac.nz>
2019-03-17 22:57:57 +0800

    index html pop song page - done

--- src/js/index_js/page2.js
+++ src/js/index_js/page2.js
@@ -1,9 +1,11 @@
 {
-  let view={
-    el:'section.page1 > .newSong > ul',
+  let view = {
+    el:'.popSongList',
+    updateTime:'.updateTime',
     template:`
     <li>
     <a href="./playSong.html?id=__id__">
+      <h1>__order__</h1>
       <div class="song-wrapper">
         <h4>__title__</h4>
         <h6 isHQ="__val__">__singer__ - __album__</h6>
@@ -16,31 +18,35 @@
     </a>
     </li>
     `,
-    render(data){//data is an array
+    renderSong(dataArr){
       let el = $(this.el);
-      data.map((data)=>{
+      let placeholders = ['id','title','singer','album']
+      dataArr.map((data,index)=>{
         let template = this.template;
-        template = template.replace('__id__',data.id||'');
-        template = template.replace('__title__',data.title||'');
-        template = template.replace('__singer__',data['singer']||'');
-        template = template.replace('__album__',data['album']||'');
+        let order = dataArr.length - index;
+        placeholders.map((i)=>{
+          template = template.replace(`__${i}__`,data[i]||'');
+        })
         if(data.isHQ==="1"){
           template = template.replace('<h6 isHQ="__val__">',"<h6 isHQ='HQ' class='active'>")
         }else{
           template = template.replace(' isHQ="__val__"','')
         }
+        template = template.replace('__order__',order);
         $(el).prepend(template);
       })
-    },
-
-  }
-  let model={
+      let updateTimeEl = $(this.updateTime)[0];
+      let curDate = new Date();
+      updateTimeEl.textContent = '更新于'+curDate.getFullYear()+'年'+(curDate.getMonth()+1)+'月'+curDate.getDate()+'日'
+    }
+  };
+  let model ={
     data:{id:'',title:'',singer:'',url:'',album:'',isHQ:''},
     db_data:[],
-    findAll(){
+    findPopSong(){
       let query = new AV.Query('Song');
       let placeholders = ['title','singer','album','isHQ','url']
-      query.find().then((res)=>{
+      return query.find().then((res)=>{
         let songItem = [];
         res.map((i)=>{
           let item = {};
@@ -51,22 +57,21 @@
           return songItem.push(item);
         })
         this.db_data = songItem;
-        console.log(this.db_data)
-        window.eventHub.emit('find',this.db_data);
-      },(err)=>{console.error(err)})
+      }).catch((err)=>{console.log(err)})
     }
-  }
-  let controller ={
-    view:null,
-    model:null,
+  };
+  let controller = {
     init(view,model){
       this.view = view;
       this.model = model;
-      this.model.findAll();
-      window.eventHub.on('find',(data)=>{
-        this.view.render(data);
-      })
+      this.bindEvent();
+    },
+    bindEvent(){
+      this.model.findPopSong()
+        .then(()=>{
+          this.view.renderSong(this.model.db_data);
+        });
     }
-  }
-  controller.init(view,model)
+  };
+  controller.init(view,model);
 }
